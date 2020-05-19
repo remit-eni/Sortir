f5c46c67416b7152f4987c4222a07ffda41a2bf8
Laurentiu36 <62551614+Laurentiu36@users.noreply.github.com>
2020-05-19 11:32:02 +0200

    entity Etat

--- src/Entity/Etat.php
+++ src/Entity/Etat.php
@@ -27,6 +27,22 @@ class Etat
      */
     private $sortie;
 
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
 
 
 
