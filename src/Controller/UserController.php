98e182caa094abeeacfb7bf12552ff55973982c8
RoiBann <manuchauveau@outlook.fr>
2020-05-19 17:16:12 +0200

    maj campus

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,24 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
+use App\Form\ProfilType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
     /**
-     * @route("/profile", name="profile_show")
+     * @Route("/profile", name="profile_show")
      */
     public function show(){
         return $this->render('user/profile.html.twig');
     }
 
     /**
-     * @route("/profile/new", name="profile_create")
+     * @Route("/profile/new", name="profile_create")
      */
-    public function create(){
-        return $this->render('user/create.html.twig');
+    public function create(EntityManagerInterface $em, Request $request){
+
+        $participant = new Participant();
+
+        $profileForm = $this->createForm(ProfilType::class, $participant);
+        $profileForm->handleRequest($request);
+
+        if ($profileForm->isSubmitted() && $profileForm->isValid()){
+            $em->persist($participant);
+            $em->flush();
+
+        }
+        return $this->render('user/create.html.twig',[
+            "profileForm"=>$profileForm->createView()
+        ]);
     }
+
 
     /**
      * @Route("/profile/{id}/edit"), name="profile_edit")
