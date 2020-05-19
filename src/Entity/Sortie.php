c0920f74cc6d87100d50febc61931f5c2c53545b
Rémi Treussart <remi.treussart2020@campus-eni.fr>
2020-05-19 11:33:12 +0200

    Entité Sortie

---
+++ src/Entity/Sortie.php
@@ -0,0 +1,267 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SortieRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SortieRepository::class)
+ */
+class Sortie
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateHeureDebut;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $duree;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateLimiteInscription;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbInscriptionsMax;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $infosSortie;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Lieu", inversedBy="sorties")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $lieu;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Etat", inversedBy="sorties")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $etat;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Participant", inversedBy="sortiesOrganisees")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $organisateur;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Participant", inversedBy="sortiesInscrites")
+     */
+    private $participants;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Campus", inversedBy="sorties")
+     */
+    private $campusOrganisateur;
+
+    public function __construct()
+    {
+        $this->participants = new ArrayCollection();
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom): void
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateHeureDebut()
+    {
+        return $this->dateHeureDebut;
+    }
+
+    /**
+     * @param mixed $dateHeureDebut
+     */
+    public function setDateHeureDebut($dateHeureDebut): void
+    {
+        $this->dateHeureDebut = $dateHeureDebut;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDuree()
+    {
+        return $this->duree;
+    }
+
+    /**
+     * @param mixed $duree
+     */
+    public function setDuree($duree): void
+    {
+        $this->duree = $duree;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateLimiteInscription()
+    {
+        return $this->dateLimiteInscription;
+    }
+
+    /**
+     * @param mixed $dateLimiteInscription
+     */
+    public function setDateLimiteInscription($dateLimiteInscription): void
+    {
+        $this->dateLimiteInscription = $dateLimiteInscription;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNbInscriptionsMax()
+    {
+        return $this->nbInscriptionsMax;
+    }
+
+    /**
+     * @param mixed $nbInscriptionsMax
+     */
+    public function setNbInscriptionsMax($nbInscriptionsMax): void
+    {
+        $this->nbInscriptionsMax = $nbInscriptionsMax;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getInfosSortie()
+    {
+        return $this->infosSortie;
+    }
+
+    /**
+     * @param mixed $infosSortie
+     */
+    public function setInfosSortie($infosSortie): void
+    {
+        $this->infosSortie = $infosSortie;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLieu()
+    {
+        return $this->lieu;
+    }
+
+    /**
+     * @param mixed $lieu
+     */
+    public function setLieu($lieu): void
+    {
+        $this->lieu = $lieu;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEtat()
+    {
+        return $this->etat;
+    }
+
+    /**
+     * @param mixed $etat
+     */
+    public function setEtat($etat): void
+    {
+        $this->etat = $etat;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOrganisateur()
+    {
+        return $this->organisateur;
+    }
+
+    /**
+     * @param mixed $organisateur
+     */
+    public function setOrganisateur($organisateur): void
+    {
+        $this->organisateur = $organisateur;
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getParticipants(): ArrayCollection
+    {
+        return $this->participants;
+    }
+
+    /**
+     * @param ArrayCollection $participants
+     */
+    public function setParticipants(ArrayCollection $participants): void
+    {
+        $this->participants = $participants;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCampusOrganisateur()
+    {
+        return $this->campusOrganisateur;
+    }
+
+    /**
+     * @param mixed $campusOrganisateur
+     */
+    public function setCampusOrganisateur($campusOrganisateur): void
+    {
+        $this->campusOrganisateur = $campusOrganisateur;
+    }
+
+
+}
