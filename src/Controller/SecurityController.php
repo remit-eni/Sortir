c5e3c4bd187b4ff0fb1dc84425c9d9fd0777aee1
Jeff <husson.jeanfrancois@gmail.com>
2020-05-22 09:38:27 +0200

    Ajout lien profile + acces control + clearPassword + redirection

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
@@ -11,9 +13,11 @@ class SecurityController extends AbstractController
     /**
      * @Route("/login", name="login")
      */
-    public function login()
+    public function login(AuthenticationUtils $authenticationUtils):Response
     {
-        return $this->render('security/login.html.twig');
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
     /**
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,61 +2,79 @@
 
 namespace App\Controller;
 
-use App\Entity\Idea;
 use App\Entity\Participant;
 use App\Form\ProfilType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class UserController extends AbstractController
 {
-
-
     /**
      * @route("/profile/new", name="profile_create")
      * @Route("/profile/{id}/edit", name="profile_edit")
      */
     public function create(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $encoder,
-                           Participant $participant = null){
-       if (!$participant){
-           $participant = new Participant();
-       }
-       $profileForm = $this->createForm(ProfilType::class, $participant);
+                           Participant $participant = null, AuthenticationUtils $authenticationUtils):Response
+    {
 
-       $profileForm->handleRequest($request);
+        if (!$participant)
+        {
+            $participant = new Participant();
+        }
 
-       if ($profileForm->isSubmitted() && $profileForm->isValid()) {
+        $profileForm = $this->createForm(ProfilType::class, $participant);
 
-           $hash = $encoder->encodePassword($participant, $participant->getPassword());
-           $participant->setPassword($hash);
+        $profileForm->handleRequest($request);
 
-               if (!$participant->getId()) {
-                   $participant->setIsAdmin(false);
-                   $participant->setIsActif(true);
-               }
-           $em->persist($participant);
-           $em->flush();
+        if ($profileForm->isSubmitted() && $profileForm->isValid()) {
 
-           return $this->redirectToRoute('login', [
-               'id' => $participant->getId()
-           ]);
-       }
+            $clearPassword = $participant->getPassword();
+            $hash = $encoder->encodePassword($participant, $participant->getPassword());
+            $participant->setPassword($hash);
 
-           return $this->render('user/create.html.twig', [
-               'profileForm'=> $profileForm->createView(),
-               'editMode'=>$participant->getId()!==null
-           ]);
+            if (!$participant->getId())
+            {
+                $participant->setIsAdmin(false);
+                $participant->setIsActif(true);
+
+                $em->persist($participant);
+                $em->flush();
+
+                $error = $authenticationUtils->getLastAuthenticationError();
+                $lastUsername = $authenticationUtils->getLastUsername();
+
+                $this->addFlash('success', 'Votre profil a bien été enregistré !');
+                return $this->render('security/login.html.twig', ['username' => $participant->getUsername(), 'clearPassword' => $clearPassword, 'last_username' => $lastUsername, 'error' => $error]);
+            } else {
+
+                    $em->persist($participant);
+                    $em->flush();
+
+                    $this->addFlash('success', 'Votre profil a bien été modifié !');
+                    return $this->redirectToRoute('home', [
+                        'id' => $participant->getId()
+                    ]);
+                    }
+        }
+
+        return $this->render('user/create.html.twig', [
+            'profileForm' => $profileForm->createView(),
+            'editMode' => $participant->getId() !== null
+        ]);
     }
 
     /**
      * @Route("/profile/{id}", name="profile_show", requirements={"id": "\d+"})
      */
-    public function show($id, Request $request){
-        $participantRepo=$this->getDoctrine()->getRepository(Participant::class);
-        $participant=$participantRepo->find($id);
-        return $this->render('user/profile.html.twig',["participant"=>$participant]);
+    public function show($id, Request $request)
+    {
+        $participantRepo = $this->getDoctrine()->getRepository(Participant::class);
+        $participant = $participantRepo->find($id);
+        return $this->render('user/profile.html.twig', ["participant" => $participant]);
     }
 }
